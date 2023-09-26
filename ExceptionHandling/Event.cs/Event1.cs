5af4dee2c779379ba241bacf293e121eaac9a851
MadhuraRadkar <madhuraradkar@gmail.com>
2023-09-26 17:28:18 +0530

    update

---
+++ ExceptionHandling/Event.cs/Event1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Event.cs
+{
+        public class event1
+        {
+            static void Main(string[] args)
+            {
+                try
+                {
+                    Student stud1 = new Student();
+                    Message msg = new Message();
+
+                    // bind event with delegate
+                    stud1.Fail += new MyDelegate(msg.FailMessage);
+                    stud1.Pass += new MyDelegate(msg.SuccessMessage);
+
+                    stud1.AcceptPercen(25);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+        }
+
+        public delegate void MyDelegate();
+        public class Student
+        {
+            
+            public event MyDelegate Fail; 
+            public event MyDelegate Pass;
+            public void AcceptPercen(double per)
+            {
+                if (per < 30)
+                {
+                    Fail(); 
+                }
+                else
+                {
+                    Pass();
+                }
+            }
+        }
+        public class Message
+        {
+            public void SuccessMessage()
+            {
+                Console.WriteLine("You are Pass ");
+            }
+            public void FailMessage()
+            {
+                Console.WriteLine("You are Fail ");
+            }
+        }
+    
+}
---
+++ ExceptionHandling/Event.cs/Event2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Event.cs
+{
+    public class event2
+    {
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Student1 stud1 = new Student1();
+
+                // bind event with delegate
+               
+                stud1.Fail += delegate ()
+                {
+                    Console.WriteLine("You are fail");
+                };
+                stud1.Pass += delegate ()
+                {
+                    Console.WriteLine("You are pass");
+                };
+
+                stud1.AcceptPercentage(65);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+
+    //
+    public delegate void MyDelegate1();
+    public class Student1
+    {
+        
+        public event MyDelegate Fail; 
+        public event MyDelegate Pass;
+        public void AcceptPercentage(double per)
+        {
+            if (per < 40)
+            {
+                Fail(); 
+            }
+            else
+            {
+                Pass();
+            }
+        }
+    }
+}
